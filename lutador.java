73bed9d183cc326db9edb619290219e5c0273aaf
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-03-25 23:53:22 -0300

    Update lutador.java

--- lutador.java
+++ lutador.java
@@ -16,7 +16,7 @@ public class lutador {
         this.iniciativa = inic; 
         return this.iniciativa; 
 
-        System.out.println("A iniciativa de fulano é " iniciativa +);
+        System.out.println("A iniciativa de fulano é " + iniciativa + "!");
     }
     public void setIniciativa(int iniciativa){
         this.iniciativa = iniciativa;
