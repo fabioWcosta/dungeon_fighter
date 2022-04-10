67806b19e7bd96b0b317e70c1e61f31c1efe79e8
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-04-09 22:39:56 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public static void main(String[] args) {
     lutador c1 = new lutador();
 
-    c1nome = "humano";
+    c1.nome = "humano";
     c1.classeArmadura = 10; 
     c1.bonusProficiencia = 2;
     c1.DV = 1;
