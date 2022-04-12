535278dee0533a2867b255408f7892a5c8119df2
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-04-11 22:00:54 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -10,11 +10,12 @@ public class principal {
         
             System.out.println("Pontos de vida do lutador:" + c1.hp);
         
+            /*
             public int setInic(){
                 double randomico = Math.random();
                 int resultado = (int) (1 + randomico * (6-1));
                 return resultado;
             }
-            
+            */
         }
 }
