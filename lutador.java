ff6fc32235b97628f9f86e46901fd7fa72113c68
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-04-26 23:56:37 -0300

    Update lutador.java

--- lutador.java
+++ lutador.java
@@ -17,6 +17,7 @@ public class lutador {
         double random = Math.random();
         int init = (int) (1 + random *(20-1));
         System.out.println("A iniciativa é: "  + init);
+        System.out.println
 
 
     }
