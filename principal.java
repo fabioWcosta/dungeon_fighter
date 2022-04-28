9a07103de30f75f319a986dff4ceb9d8bd7f90b2
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-04-28 00:01:17 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,10 +1,10 @@
 public class principal {
         public static void main(String[] args) {
-            lutador c1 = new  lutador();
+            lutador llllllll1 = new  lutador();
         
             
-            c1.nome = "humano";
-            c1.classeArmadura = 10; 
+            l1.nome = "humano";
+            l1.classeArmadura = 10; 
             c1.bonusProficiencia = 2;
             c1.DV = 1;
             c1.hp = 10;
