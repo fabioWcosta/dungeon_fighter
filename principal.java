6d7e302d12255fd0941708942785dcf13e54cd2c
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-04-28 00:00:16 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,6 +1,6 @@
 public class principal {
         public static void main(String[] args) {
-            lutador c1 = new  lutador!();
+            lutador c1 = new  lutador();
         
             
             c1.nome = "humano";
