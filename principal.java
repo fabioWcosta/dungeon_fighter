c058b95a9ee4f227c29c55b851aaf4576ef485c7
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-10 23:58:39 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,7 +1,7 @@
 public class principal {
         public static void main(String[] args) {
 
-            Lutador l = new lutador ("humano", 14, 2, 1, 10, "espada longa", 5, 2);
+            Lutador l = new Lutador ("humano", 14, 2, 1, 10, "espada longa", 5, 2);
             
             //Lutador l[] = new Lutador [5];
             //l [1] = {"humano", 10, 2, 1, 10};
