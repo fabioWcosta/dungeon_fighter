65daf0db7f258aa6f0a11519fb724297c13e2a08
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-05-12 23:56:26 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -3,7 +3,7 @@ public class principal {
 
             lutador l[] = new lutador [5];
             
-            lutador l[1] = ("humano", 14, 2, 1, 10, "espada longa", 5, 2);
+            lutador l[0] = ("humano", 14, 2, 1, 10, "espada longa", 5, 2);
 
             System.out.println("Pontos de vida do lutador:" + l[1].hp);
             System.out.println("Iniciativa do lutador:" + l[1].iniciativa);
